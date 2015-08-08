0d7812bb8f2f7dc9d641283093f88ea816b37178
pedroteosousa <pedroteosousa@gmail.com>
2015-08-08 18:10:20 -0300

    Initial Commit

---
+++ GHT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GHT
+//
+//  Created by Pedro Sousa on 8/8/15.
+//  Copyright (c) 2015 Pedro Sousa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
